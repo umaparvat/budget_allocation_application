098ba3f4dbbb2902ac9b459698d0f75841f78d8b
Umaparvathy Kaliappan <ka.umaparvathi.cse@gmail.com>
2023-04-23 23:12:49 +0530

    added budget react code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import ExpensesTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AllocationForm from "./components/AllocationForm";
 
-function App() {
+function App(props){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='mt-3'>Company's Budget Allocation</h1>
+      <div className='row mt-3'>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget/>
+          </div>
+          <div className="col-sm">
+            <Remaining/>
+          </div>
+          <div className="col-sm">
+            <ExpensesTotal/>
+          </div>
+        </div>
+        <h3 className='mt-3'>Allocation</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
+        </div>
+        <h3 className='mt-3'>Change allocation</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AllocationForm/>
+          </div>
+
+        </div>
+      </div>
+      
+
     </div>
-  );
+
+  )
 }
 
 export default App;
---
+++ src/components/Budget.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
+const Budget = () => {
+    const { budget } = useContext(AppContext)
+    return(
+        <div className="alert alert-secondary">
+            <span>budget: £{budget}</span>
+
+        </div>
+    )
+}
+
+export default Budget;
